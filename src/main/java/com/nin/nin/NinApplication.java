f5adb25d6a5b016d06480bf187221830c0efb0d7
LONGBRIDGE <chioma.chukelu@longbridgetech.com>
2021-02-14 13:13:34 +0100

    added

---
+++ src/main/java/com/nin/nin/NinApplication.java
@@ -0,0 +1,13 @@
+package com.nin.nin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nin/nin/NinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nin.nin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
